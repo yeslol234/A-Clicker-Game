51d4e7ff80a683c73b604c283fcf7f58d288bd62
yeslol234 <72095827+yeslol234@users.noreply.github.com>
2021-08-05 17:02:46 -0700

    Added Stuff

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ let bitsperclick = 1;
 // Functions
 function increment() {
  bits += bitsperclick
-  
+  document.getElementById("bitscount").innerText = bits;
 }
+setInterval(function() {
+ bits += bitspersecond
+ document.getElementById("bitscount").innerText = bits;
+},1000)
 // Upgrades, Buildings, etc.
-
+let MoreBitsPerClickCount = 0
+let MoreBitsPerSecondCount = 0
+let MoreBitsPerClickPrice = 25
+let MoreBitsPerSecondPrice 50
+// other functions anauownouwnnn
+function BuyMoreBits() {
+ 
+}
